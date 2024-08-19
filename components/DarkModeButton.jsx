d9118af69e8036b33d9361351e6203f0bfe9ffb4
Tobias von Essen <vonessen@mailbox.org>
2024-08-19 10:02:20 +0200

    fix: better auto dark mode icon

--- components/DarkModeButton.jsx
+++ components/DarkModeButton.jsx
@@ -9,15 +9,63 @@ import {
   DropdownItem,
   Button,
 } from "@nextui-org/react";
-import { Moon, LightbulbFilament, Sun } from "@phosphor-icons/react";
+import { Moon, LightningA, Sun } from "@phosphor-icons/react";
+import auto from "public/auto.svg";
+import Image from "next/image";
 
 const DarkModeButton = () => {
   const [theme, setTheme] = useLocalStorage("theme", "auto");
 
+  const Auto = () => {
+    return (
+      <svg
+        width="24px"
+        height="24px"
+        viewBox="0 0 256 256"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        className="stroke-current"
+        style={{
+          fillRule: "evenodd",
+          clipRule: "evenodd",
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+        }}
+      >
+        <path
+          d="M128,40L128,16"
+          style={{ fill: "none", fillRule: "nonzero", strokeWidth: "16px" }}
+        />
+        <path
+          d="M74.97,146.03C73.045,140.371 72,134.307 72,128C72,97.093 97.093,72 128,72C134.307,72 140.371,73.045 146.03,74.97"
+          style={{ fill: "none", fillRule: "nonzero", strokeWidth: "16px" }}
+        />
+        <path
+          d="M64,64L48,48"
+          style={{ fill: "none", fillRule: "nonzero", strokeWidth: "16px" }}
+        />
+        <path
+          d="M208,48L48,208"
+          style={{ fill: "none", fillRule: "nonzero", strokeWidth: "16px" }}
+        />
+        <path
+          d="M40,128L16,128"
+          style={{ fill: "none", fillRule: "nonzero", strokeWidth: "16px" }}
+        />
+        <g transform="matrix(1,0,0,1,-2.30216,-2.30216)">
+          <path
+            d="M154.983,140.621C168.466,147.873 183.834,151.999 200.091,151.999C209.509,151.999 218.875,150.615 227.89,147.89C215.628,188.208 178.185,215.956 136.044,215.956C119.812,215.956 104.467,211.839 91.003,204.602"
+            style={{ fill: "none", fillRule: "nonzero", strokeWidth: "16px" }}
+          />
+        </g>
+      </svg>
+    );
+  };
+
   const themes = React.useMemo(
     () => ({
       auto: {
-        icon: <LightbulbFilament size="24" weight="light" />,
+        icon: <Auto />,
         label: "Auto",
       },
       light: {
